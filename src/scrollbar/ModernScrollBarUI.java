4a672fa8ad77ebb4e5ef3618d83b5e5008f76f44
laing raven <laingraven.java@gmail.com>
2021-10-03 15:42:41 +0700

    add scrollbar custom

---
+++ src/scrollbar/ModernScrollBarUI.java
@@ -0,0 +1,103 @@
+package scrollbar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JScrollBar;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
+public class ModernScrollBarUI extends BasicScrollBarUI {
+
+    private final int THUMB_SIZE = 40;
+
+    @Override
+    protected Dimension getMaximumThumbSize() {
+        if (scrollbar.getOrientation() == JScrollBar.VERTICAL) {
+            return new Dimension(0, THUMB_SIZE);
+        } else {
+            return new Dimension(THUMB_SIZE, 0);
+        }
+    }
+
+    @Override
+    protected Dimension getMinimumThumbSize() {
+        if (scrollbar.getOrientation() == JScrollBar.VERTICAL) {
+            return new Dimension(0, THUMB_SIZE);
+        } else {
+            return new Dimension(THUMB_SIZE, 0);
+        }
+    }
+
+    @Override
+    protected JButton createIncreaseButton(int i) {
+        return new ScrollBarButton();
+    }
+
+    @Override
+    protected JButton createDecreaseButton(int i) {
+        return new ScrollBarButton();
+    }
+
+    @Override
+    protected void paintTrack(Graphics grphcs, JComponent jc, Rectangle rctngl) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        int orientation = scrollbar.getOrientation();
+        int size;
+        int x;
+        int y;
+        int width;
+        int height;
+        if (orientation == JScrollBar.VERTICAL) {
+            size = rctngl.width / 2;
+            x = rctngl.x + ((rctngl.width - size) / 2);
+            y = rctngl.y;
+            width = size;
+            height = rctngl.height;
+        } else {
+            size = rctngl.height / 2;
+            y = rctngl.y + ((rctngl.height - size) / 2);
+            x = 0;
+            width = rctngl.width;
+            height = size;
+        }
+        g2.setColor(new Color(240, 240, 240));
+        g2.fillRect(x, y, width, height);
+    }
+
+    @Override
+    protected void paintThumb(Graphics grphcs, JComponent jc, Rectangle rctngl) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        int x = rctngl.x;
+        int y = rctngl.y;
+        int width = rctngl.width;
+        int height = rctngl.height;
+        if (scrollbar.getOrientation() == JScrollBar.VERTICAL) {
+            y += 8;
+            height -= 16;
+        } else {
+            x += 8;
+            width -= 16;
+        }
+        g2.setColor(scrollbar.getForeground());
+        g2.fillRoundRect(x, y, width, height, 10, 10);
+    }
+
+    private class ScrollBarButton extends JButton {
+
+        public ScrollBarButton() {
+            setBorder(BorderFactory.createEmptyBorder());
+        }
+
+        @Override
+        public void paint(Graphics grphcs) {
+        }
+    }
+}
---
+++ src/scrollbar/ScrollBarCustom.java
@@ -0,0 +1,15 @@
+package scrollbar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JScrollBar;
+
+public class ScrollBarCustom extends JScrollBar {
+
+    public ScrollBarCustom() {
+        setUI(new ModernScrollBarUI());
+        setPreferredSize(new Dimension(8, 8));
+        setForeground(new Color(48, 144, 216));
+        setBackground(Color.WHITE);
+    }
+}
